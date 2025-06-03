884aa5ba3a2733d0aa811014f9a420fdaf401e54
sarath2606 <92908086+Tejas1018@users.noreply.github.com>
2025-06-03 19:27:10 +0530

    Valid Anagrams LC : 242

---
+++ src/main/java/com/service/strings/validAnagrams.java
@@ -0,0 +1,37 @@
+package com.service.strings;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class validAnagrams {
+
+    public boolean isAnagram(String s, String t) {
+        int n = s.length();
+        int m = t.length();
+        if(n != m) return false;
+        Map<Character,Integer> map = new HashMap<>();
+        for(int i = 0 ; i < n ; i++){
+            char ch = s.charAt(i);
+            if(map.containsKey(ch)){
+                map.put(ch,map.get(ch)+1);
+            }
+            else{
+                map.put(ch,1);
+            }
+        }
+
+        for(int i = 0; i < m ; i++){
+            char ch = t.charAt(i);
+            if(!map.containsKey(ch)){
+                return false;
+            }
+            else{
+                map.put(ch,map.get(ch)-1);
+                if(map.get(ch) == 0){
+                    map.remove(ch);
+                }
+            }
+        }
+        return true;
+    }
+}
